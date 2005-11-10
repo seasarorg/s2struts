f369ca0ad78f0544ac9e0add6077c65a02b171ed
kanagashi <kanagashi@c287a166-f701-0410-b539-9faeb104086e>
2005-11-10 12:31:55 +0000

    git-svn-id: https://www.seasar.org/svn/s2struts/trunk/s2struts-tiger@110 c287a166-f701-0410-b539-9faeb104086e

---
+++ src/testcase/org/seasar/struts/factory/TigerActionAnnotationHandlerTest.java
@@ -0,0 +1,26 @@
+package org.seasar.struts.factory;
+
+import org.seasar.extension.unit.S2TestCase;
+import org.seasar.framework.beans.BeanDesc;
+import org.seasar.framework.beans.PropertyDesc;
+import org.seasar.framework.beans.factory.BeanDescFactory;
+import org.seasar.struts.action.ExportSessionPropertyActionImpl;
+import org.seasar.struts.config.ActionPropertyConfig;
+
+public class TigerActionAnnotationHandlerTest extends S2TestCase {
+
+    private ActionAnnotationHandler annHandler;
+    
+    public void setUp() {
+        annHandler = ActionAnnotationHandlerFactory.getAnnotationHandler();
+    }
+    
+    public void testCreateStrutsActionPropertyConfigByExportToSession() throws Exception {
+        BeanDesc beanDesc = BeanDescFactory.getBeanDesc(ExportSessionPropertyActionImpl.class);
+        PropertyDesc propertyDesc = beanDesc.getPropertyDesc("qux");
+        ActionPropertyConfig config = annHandler.createActionPropertyConfig(beanDesc, propertyDesc);
+        assertNotNull(config);
+        assertEquals(true, config.isSessionScope());
+    }
+    
+}
