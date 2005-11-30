4fd33cebad944ce50c8896f50055701f982ac8a1
kanagashi <kanagashi@c287a166-f701-0410-b539-9faeb104086e>
2005-11-30 13:54:48 +0000

    git-svn-id: https://www.seasar.org/svn/s2struts/trunk/s2struts-example@225 c287a166-f701-0410-b539-9faeb104086e

--- WEB-INF/src/org/seasar/struts/examples/indexed/IndexedDto.java
+++ WEB-INF/src/org/seasar/struts/examples/indexed/IndexedDto.java
@@ -1,20 +1,32 @@
 package org.seasar.struts.examples.indexed;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.seasar.struts.validator.annotation.tiger.Args;
+import org.seasar.struts.validator.annotation.tiger.Required;
 
 public class IndexedDto {
 
     private List<ChildDto> children = new ArrayList<ChildDto>();
 
     private String[] array = new String[3];
-    
+
     private Map map = new HashMap();
 
     private List<String> values = new ArrayList<String>();
+
+    public ChildDto[] getChildren() {
+        return children.toArray(new ChildDto[children.size()]);
+    }
     
+    public void setChildren(ChildDto[] children) {
+        this.children = Arrays.asList(children);
+    }
+
     public ChildDto getChild(int index) {
         while (children.size() < index + 1) {
             children.add(new ChildDto());
@@ -26,21 +38,33 @@ public class IndexedDto {
         return array;
     }
 
+    @Required
+    @Args(keys = "array", resource = false)
     public void setArray(String[] array) {
         this.array = array;
     }
-    
+
     public Map getMap() {
         return map;
     }
-    
+
+    public String[] getValues() {
+        return values.toArray(new String[values.size()]);
+    }
+
+    @Required
+    @Args(keys = "list", resource = false)
+    public void setValues(String[] values) {
+        this.values = Arrays.asList(values);
+    }
+
     public String getValues(int index) {
         while (values.size() < index + 1) {
             values.add("");
         }
         return values.get(index);
     }
-    
+
     public void setValues(int index, String value) {
         while (values.size() < index + 1) {
             values.add("");
@@ -48,11 +72,7 @@ public class IndexedDto {
         values.remove(index);
         values.add(index, value);
     }
-    
-    public String[] getValues() {
-        return values.toArray(new String[values.size()]);
-    }
-    
+
     public String toString() {
         StringBuffer buf = new StringBuffer("[");
         buf.append(children).append(",");
