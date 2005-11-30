045cf8af80df889c6845e80ec655cb98d618202d
kanagashi <kanagashi@c287a166-f701-0410-b539-9faeb104086e>
2005-11-30 13:13:28 +0000

    git-svn-id: https://www.seasar.org/svn/s2struts/trunk/s2struts-backport175@222 c287a166-f701-0410-b539-9faeb104086e

---
+++ src/test/java/org/seasar/struts/form/ValidatorAnnotationGrandchildForm.java
@@ -0,0 +1,21 @@
+package org.seasar.struts.form;
+
+public class ValidatorAnnotationGrandchildForm {
+
+    /**
+     * @org.seasar.struts.validator.annotation.backport175.Required
+     * @org.seasar.struts.validator.annotation.backport175.Args(keys="GrandchildRequired", resource=false)
+     */
+    public void setRequired(String required) {
+    }
+    
+    /**
+     * @org.seasar.struts.validator.annotation.backport175.Args(keys="GrandchildInteger", resource=false)
+     */
+    public void setInteger(int integer) {
+    }
+    
+    public void setNoValidate(String noValidate) {
+    }
+
+}
