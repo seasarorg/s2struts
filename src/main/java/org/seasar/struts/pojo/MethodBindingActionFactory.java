997250bae8e02f8409014bfb5f3f775163d8d18b
kanagashi <kanagashi@c287a166-f701-0410-b539-9faeb104086e>
2006-05-26 08:47:02 +0000

    git-svn-id: https://www.seasar.org/svn/s2struts/branches/s2struts-idea@372 c287a166-f701-0410-b539-9faeb104086e

--- src/main/java/org/seasar/struts/pojo/MethodBindingActionFactory.java
+++ src/main/java/org/seasar/struts/pojo/MethodBindingActionFactory.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.seasar.struts.pojo;
 
 import java.util.Enumeration;
@@ -8,6 +23,10 @@ import org.apache.struts.action.ActionServlet;
 import org.seasar.struts.util.IndexedUtil;
 import org.seasar.struts.util.S2StrutsContextUtil;
 
+/**
+ * 
+ * @author Katsuhiko Nagashima
+ */
 public class MethodBindingActionFactory {
 
     public static MethodBindingAction createMethodBindingAction(HttpServletRequest request,
--- src/main/java/org/seasar/struts/pojo/processor/ProcessPojoActionCreateInterceptor.java
+++ src/main/java/org/seasar/struts/pojo/processor/ProcessPojoActionCreateInterceptor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.seasar.struts.pojo.processor;
 
 import org.aopalliance.intercept.MethodInvocation;
@@ -6,6 +21,10 @@ import org.seasar.framework.aop.interceptors.AbstractInterceptor;
 import org.seasar.struts.pojo.PojoProcessAction;
 import org.seasar.struts.util.ClassRegister;
 
+/**
+ * 
+ * @author Katsuhiko Nagashima
+ */
 public class ProcessPojoActionCreateInterceptor extends AbstractInterceptor {
 
     private static final long serialVersionUID = -8477606812838214606L;
