034418de122bb5e507a37c9ddcf6a9c7cf1f9214
kanagashi <kanagashi@c287a166-f701-0410-b539-9faeb104086e>
2006-05-30 13:36:58 +0000

    git-svn-id: https://www.seasar.org/svn/s2struts/trunk@415 c287a166-f701-0410-b539-9faeb104086e

---
+++ s2struts-hotdeploy-demo/WEB-INF/src/examples/hotdeploy/webpojo/ModForm2.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package examples.hotdeploy.webpojo;
+
+import java.io.Serializable;
+
+/**
+ * @author Katsuhiko Nagashima
+ */
+public class ModForm2 implements Serializable {
+
+    private static final long serialVersionUID = 1807022024741371134L;
+
+    private String arg1;
+
+    private String arg2;
+
+    private int result;
+
+    public String getArg1() {
+        return arg1;
+    }
+
+    // @Required
+    // @IntegerType
+    // @Args(keys = "modForm.arg1")
+    public void setArg1(String arg1) {
+        this.arg1 = arg1;
+    }
+
+    public int getIntArg1() {
+        try {
+            return Integer.parseInt(arg1);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    public String getArg2() {
+        return arg2;
+    }
+
+    // @Required
+    // @IntegerType
+    // @Args(keys = "modForm.arg2")
+    public void setArg2(String arg2) {
+        this.arg2 = arg2;
+    }
+
+    public int getIntArg2() {
+        try {
+            return Integer.parseInt(arg2);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public String toString() {
+        return "[" + arg1 + "," + arg2 + "]";
+    }
+
+}
