1c9b3755311ec6b971d27bd4beb9dd7f160e1fa2
kanagashi <kanagashi@c287a166-f701-0410-b539-9faeb104086e>
2006-07-11 13:20:02 +0000

    git-svn-id: https://www.seasar.org/svn/s2struts/trunk@489 c287a166-f701-0410-b539-9faeb104086e

---
+++ s2struts/src/main/java/org/seasar/struts/pojo/processor/ProcessCheckboxPopulateInterceptor.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.struts.pojo.processor;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.struts.upload.MultipartRequestWrapper;
+import org.seasar.framework.aop.interceptors.AbstractInterceptor;
+import org.seasar.struts.Constants;
+import org.seasar.struts.servlet.http.S2ServletRequestWrapper;
+
+/**
+ * 
+ * @author Katsuhiko Nagashima
+ *
+ */
+public class ProcessCheckboxPopulateInterceptor extends AbstractInterceptor {
+
+    private static final long serialVersionUID = -118149589979795316L;
+
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        HttpServletRequest request = (HttpServletRequest) invocation.getArguments()[0];
+        
+        if (request instanceof MultipartRequestWrapper) {
+            invocation.proceed();
+            Map parameters = getCheckBoxParameters(request);
+            if (!parameters.isEmpty()) {
+                S2ServletRequestWrapper s2request = new S2ServletRequestWrapper(
+                        ((MultipartRequestWrapper) request).getRequest());
+                addParameter(s2request, parameters);
+                invocation.getArguments()[0] = new MultipartRequestWrapper(s2request);
+                invocation.proceed();
+            }
+        } else {
+            Map parameters = getCheckBoxParameters(request);
+            if (!parameters.isEmpty()) {
+                S2ServletRequestWrapper s2request = new S2ServletRequestWrapper(request);
+                addParameter(s2request, parameters);
+                invocation.getArguments()[0] = s2request;
+            }
+            invocation.proceed();
+        }
+        
+        return null;
+    }
+
+    private Map getCheckBoxParameters(HttpServletRequest request) {
+        Map result = new HashMap();
+        for (Iterator i = request.getParameterMap().keySet().iterator(); i.hasNext();) {
+            String paramName = (String) i.next();
+            if (paramName.startsWith(Constants.CHECKBOX_NAME)) {
+                String checkboxParamName = paramName.substring(Constants.CHECKBOX_NAME.length());
+                String checkboxValue = request.getParameter(checkboxParamName);
+                if (checkboxValue == null) {
+                    result.put(checkboxParamName, Boolean.FALSE.toString());
+                }
+            }
+        }
+        return result;
+    }
+
+    private void addParameter(S2ServletRequestWrapper request, Map parameters) {
+        for (Iterator i = parameters.entrySet().iterator(); i.hasNext();) {
+            Map.Entry parameter = (Map.Entry) i.next();
+            request.addParameterValue((String) parameter.getKey(), (String) parameter.getValue());
+        }
+    }
+
+}
