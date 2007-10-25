b3f3289a3435a18c760393f717a4dbc70c8689ff
nakamura-to <toshihiro.nakamura@gmail.com>
2007-10-25 11:13:37 +0000

    r:974の修正漏れに対応しました。
    git-svn-id: https://www.seasar.org/svn/s2struts/branches/s2struts-tiger-1.2.3-branch@979 c287a166-f701-0410-b539-9faeb104086e

--- src/test/java/org/seasar/struts/config/ClassComparatorTest.java
+++ src/test/java/org/seasar/struts/config/ClassComparatorTest.java
@@ -15,38 +15,37 @@ import org.seasar.struts.form.ValidatorAnnotationForm;
  * @author Satoshi Kimura
  */
 public class ClassComparatorTest extends TestCase {
-    
-    @SuppressWarnings("unchecked")
-    public void testCompare() {
-        List<Class> list = new ArrayList<Class>();
-        list.add(Action.class);
-        list.add(StrutsConfigAnnotationAction.class);
-        list.add(ValidatorAnnotationForm.class);
-        list.add(ActionForm.class);
-        Object[] objects = list.toArray();
-        Arrays.sort(objects, new ClassComparator());
-        assertEquals(StrutsConfigAnnotationAction.class, objects[0]);
-        assertEquals(ValidatorAnnotationForm.class, objects[1]);
-        assertEquals(Action.class, objects[2]);
-        assertEquals(ActionForm.class, objects[3]);
 
-    }
-    
-    @SuppressWarnings("unchecked")
-    public void testSort() {
-        List<Class> list = new ArrayList<Class>();
-        list.add(Action.class);
-        list.add(StrutsConfigAnnotationAction.class);
-        list.add(ValidatorAnnotationForm.class);
-        list.add(ActionForm.class);
-        
-        list = ClassComparator.sort(list);
+	@SuppressWarnings("unchecked")
+	public void testCompare() {
+		List<Class> list = new ArrayList<Class>();
+		list.add(Action.class);
+		list.add(StrutsConfigAnnotationAction.class);
+		list.add(ValidatorAnnotationForm.class);
+		list.add(ActionForm.class);
+		Object[] objects = list.toArray();
+		Arrays.sort(objects, new ClassComparator());
+		assertEquals(Action.class, objects[0]);
+		assertEquals(StrutsConfigAnnotationAction.class, objects[1]);
+		assertEquals(ValidatorAnnotationForm.class, objects[2]);
+		assertEquals(ActionForm.class, objects[3]);
 
-        assertEquals(StrutsConfigAnnotationAction.class, list.get(0));
-        assertEquals(ValidatorAnnotationForm.class, list.get(1));
-        assertEquals(Action.class, list.get(2));
-        assertEquals(ActionForm.class, list.get(3));
-}
+	}
 
+	@SuppressWarnings("unchecked")
+	public void testSort() {
+		List<Class> list = new ArrayList<Class>();
+		list.add(Action.class);
+		list.add(StrutsConfigAnnotationAction.class);
+		list.add(ValidatorAnnotationForm.class);
+		list.add(ActionForm.class);
+
+		list = ClassComparator.sort(list);
+
+		assertEquals(Action.class, list.get(0));
+		assertEquals(StrutsConfigAnnotationAction.class, list.get(1));
+		assertEquals(ValidatorAnnotationForm.class, list.get(2));
+		assertEquals(ActionForm.class, list.get(3));
+	}
 
 }
--- src/test/java/org/seasar/struts/factory/TigerStrutsConfigAnnotationHandlerTest.java
+++ src/test/java/org/seasar/struts/factory/TigerStrutsConfigAnnotationHandlerTest.java
@@ -5,6 +5,8 @@ import java.lang.reflect.Field;
 import org.seasar.extension.unit.S2TestCase;
 import org.seasar.struts.action.StrutsConfigAnnotationAction;
 import org.seasar.struts.action.StrutsConfigAnnotationActionImpl;
+import org.seasar.struts.config.NullStrutsActionConfig;
+import org.seasar.struts.config.NullStrutsActionFormConfig;
 import org.seasar.struts.config.StrutsActionConfig;
 import org.seasar.struts.config.StrutsActionFormConfig;
 import org.seasar.struts.config.StrutsActionForwardConfig;
@@ -14,64 +16,71 @@ import org.seasar.struts.form.StrutsConfigAnnotationForm;
 /**
  * 
  * @author Katsuhiko Nagashima
- *
+ * 
  */
 public class TigerStrutsConfigAnnotationHandlerTest extends S2TestCase {
-    
-    private StrutsConfigAnnotationHandler annHandler;
-    
-    public void setUp() {
-        annHandler = StrutsConfigAnnotationHandlerFactory.getAnnotationHandler();
-    }
-    
-    public void testCreateStrutsActionConfig() {
-        StrutsActionConfig config = annHandler.createStrutsActionConfig(StrutsConfigAnnotationAction.class);
-        assertNotNull(config);
-        assertEquals("testpath", config.path());
-        assertEquals("testname", config.name());
-        assertEquals("testinput", config.input());
-        assertEquals("testparameter", config.parameter());
-        assertEquals("testattribute", config.attribute());
-        assertEquals("testforward", config.forward());
-        assertEquals("testinclude", config.include());
-        assertEquals("testprefix", config.prefix());
-        assertEquals("testsuffix", config.suffix());
-        assertEquals("testroles", config.roles());
-        assertEquals("request", config.scope());
-        assertEquals(true, config.validate());
-        assertEquals(false, config.unknown());
-        assertEquals(true, config.cancellable());
-    }
-    
-    public void testNotCreateStrutsActionConfig() {
-        StrutsActionConfig config = annHandler.createStrutsActionConfig(StrutsConfigAnnotationActionImpl.class);
-        assertNull(config);
-    }
-    
-    public void testCreateStrutsActionForwardConfig() throws Exception {
-        Field field = StrutsConfigAnnotationAction.class.getField("SUCCESS");
-        StrutsActionForwardConfig config = annHandler.createStrutsActionForwardConfig(field);
-        assertNotNull(config);
-        assertEquals("/test.jsp", config.path());
-        assertEquals(false, config.redirect());
-    }
-    
-    public void testNotCreateStrutsActionForwardConfig() throws Exception {
-        Field field = StrutsConfigAnnotationAction.class.getField("CONST");
-        StrutsActionForwardConfig config = annHandler.createStrutsActionForwardConfig(field);
-        assertNull(config);
-    }
-    
-    public void testCreateActionFormConfig() {
-        StrutsActionFormConfig config = annHandler.createStrutsActionFormConfig(StrutsConfigAnnotationForm.class);
-        assertNotNull(config);
-        assertEquals("testFormName", config.name());
-        assertEquals(false, config.restricted());
-    }
 
-    public void testNotCreateActionFormConfig() {
-        StrutsActionFormConfig config = annHandler.createStrutsActionFormConfig(NoStrutsConfigAnnotationForm.class);
-        assertNull(config);
-    }
+	private StrutsConfigAnnotationHandler annHandler;
+
+	public void setUp() {
+		annHandler = StrutsConfigAnnotationHandlerFactory
+				.getAnnotationHandler();
+	}
+
+	public void testCreateStrutsActionConfig() {
+		StrutsActionConfig config = annHandler
+				.createStrutsActionConfig(StrutsConfigAnnotationAction.class);
+		assertNotNull(config);
+		assertEquals("testpath", config.path());
+		assertEquals("testname", config.name());
+		assertEquals("testinput", config.input());
+		assertEquals("testparameter", config.parameter());
+		assertEquals("testattribute", config.attribute());
+		assertEquals("testforward", config.forward());
+		assertEquals("testinclude", config.include());
+		assertEquals("testprefix", config.prefix());
+		assertEquals("testsuffix", config.suffix());
+		assertEquals("testroles", config.roles());
+		assertEquals("request", config.scope());
+		assertEquals(true, config.validate());
+		assertEquals(false, config.unknown());
+		assertEquals(true, config.cancellable());
+	}
+
+	public void testNotCreateStrutsActionConfig() {
+		StrutsActionConfig config = annHandler
+				.createStrutsActionConfig(StrutsConfigAnnotationActionImpl.class);
+		assertEquals(NullStrutsActionConfig.INSTANCE, config);
+	}
+
+	public void testCreateStrutsActionForwardConfig() throws Exception {
+		Field field = StrutsConfigAnnotationAction.class.getField("SUCCESS");
+		StrutsActionForwardConfig config = annHandler
+				.createStrutsActionForwardConfig(field);
+		assertNotNull(config);
+		assertEquals("/test.jsp", config.path());
+		assertEquals(false, config.redirect());
+	}
+
+	public void testNotCreateStrutsActionForwardConfig() throws Exception {
+		Field field = StrutsConfigAnnotationAction.class.getField("CONST");
+		StrutsActionForwardConfig config = annHandler
+				.createStrutsActionForwardConfig(field);
+		assertNull(config);
+	}
+
+	public void testCreateActionFormConfig() {
+		StrutsActionFormConfig config = annHandler
+				.createStrutsActionFormConfig(StrutsConfigAnnotationForm.class);
+		assertNotNull(config);
+		assertEquals("testFormName", config.name());
+		assertEquals(false, config.restricted());
+	}
+
+	public void testNotCreateActionFormConfig() {
+		StrutsActionFormConfig config = annHandler
+				.createStrutsActionFormConfig(NoStrutsConfigAnnotationForm.class);
+		assertEquals(NullStrutsActionFormConfig.INSTANCE, config);
+	}
 
 }
