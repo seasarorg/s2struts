08db48ad26b49d8f127d1a250443292023de61c5
nakamura-to <toshihiro.nakamura@gmail.com>
2008-03-08 17:13:13 +0000

    [STRUTS-62]METHOD_BINDINGアノテーションを使用可能にする。[Seasar-user:13180] に対応しました。 https://www.seasar.org/issues/browse/STRUTS-62
    git-svn-id: https://www.seasar.org/svn/s2struts/branches/s2struts-tiger-1.2.3-branch@1070 c287a166-f701-0410-b539-9faeb104086e

--- src/test/java/org/seasar/struts/factory/TigerActionAnnotationHandlerTest.java
+++ src/test/java/org/seasar/struts/factory/TigerActionAnnotationHandlerTest.java
@@ -1,34 +1,55 @@
 package org.seasar.struts.factory;
 
+import java.lang.reflect.Method;
+
 import org.seasar.extension.unit.S2TestCase;
 import org.seasar.framework.beans.BeanDesc;
 import org.seasar.framework.beans.PropertyDesc;
 import org.seasar.framework.beans.factory.BeanDescFactory;
 import org.seasar.struts.action.ActionAnnotationActionImpl;
+import org.seasar.struts.annotation.tiger.BindingMethod;
 import org.seasar.struts.config.ActionPropertyConfig;
 
 public class TigerActionAnnotationHandlerTest extends S2TestCase {
 
-    private ActionAnnotationHandler annHandler;
-    
-    public void setUp() {
-        annHandler = ActionAnnotationHandlerFactory.getAnnotationHandler();
-    }
-    
-    public void testSessionScopeProperty() throws Exception {
-        BeanDesc beanDesc = BeanDescFactory.getBeanDesc(ActionAnnotationActionImpl.class);
-        PropertyDesc propertyDesc = beanDesc.getPropertyDesc("bar");
-        ActionPropertyConfig config = annHandler.createActionPropertyConfig(beanDesc, propertyDesc);
-        assertNotNull(config);
-        assertEquals(true, config.isSessionScope());
-    }
-    
-    public void testRequestScopeProperty() throws Exception {
-        BeanDesc beanDesc = BeanDescFactory.getBeanDesc(ActionAnnotationActionImpl.class);
-        PropertyDesc propertyDesc = beanDesc.getPropertyDesc("foo");
-        ActionPropertyConfig config = annHandler.createActionPropertyConfig(beanDesc, propertyDesc);
-        assertNotNull(config);
-        assertEquals(false, config.isSessionScope());
-    }
-    
+	private ActionAnnotationHandler annHandler;
+
+	public void setUp() {
+		annHandler = ActionAnnotationHandlerFactory.getAnnotationHandler();
+	}
+
+	public void testSessionScopeProperty() throws Exception {
+		BeanDesc beanDesc = BeanDescFactory
+				.getBeanDesc(ActionAnnotationActionImpl.class);
+		PropertyDesc propertyDesc = beanDesc.getPropertyDesc("bar");
+		ActionPropertyConfig config = annHandler.createActionPropertyConfig(
+				beanDesc, propertyDesc);
+		assertNotNull(config);
+		assertEquals(true, config.isSessionScope());
+	}
+
+	public void testRequestScopeProperty() throws Exception {
+		BeanDesc beanDesc = BeanDescFactory
+				.getBeanDesc(ActionAnnotationActionImpl.class);
+		PropertyDesc propertyDesc = beanDesc.getPropertyDesc("foo");
+		ActionPropertyConfig config = annHandler.createActionPropertyConfig(
+				beanDesc, propertyDesc);
+		assertNotNull(config);
+		assertEquals(false, config.isSessionScope());
+	}
+
+	public void testGetPath() throws Exception {
+		Method method = Hoge.class.getMethod("execute", new Class[] {});
+		String path = annHandler.getPath(method);
+		assertEquals("/hoge", path);
+	}
+
+	public static class Hoge {
+
+		@BindingMethod(path = "/hoge")
+		public String execute() {
+			return null;
+		}
+	}
+
 }
